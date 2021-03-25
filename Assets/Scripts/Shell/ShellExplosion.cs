9eb3d436bc765373904539744830d86e7ba2e025
BaconLover307 <kresnadi30@gmail.com>
2021-03-25 16:45:26 +0700

    Fix bug shell not working properly

--- Assets/Scripts/Shell/ShellExplosion.cs
+++ Assets/Scripts/Shell/ShellExplosion.cs
@@ -12,24 +12,36 @@ public class ShellExplosion : NetworkBehaviour, IPooledObject
     ParticleSystem m_ExplosionParticles;
 
 
+    [Server]
     public void OnObjectSpawn()
     {
         m_ExplosionParticles = ObjectPooler.Instance.SpawnFromPool("ShellExplosion", gameObject.transform.position).GetComponent<ParticleSystem>();
-        Invoke("Deactivate", m_MaxLifeTime);
+        Invoke(nameof(Deactivate), m_MaxLifeTime);
+        NetworkServer.Spawn(m_ExplosionParticles.gameObject);
     }
 
-
+    [Server]
     private void Deactivate()
     {
         gameObject.SetActive(false);
         m_ExplosionParticles.gameObject.SetActive(false);
+        NetworkServer.UnSpawn(gameObject);
+        NetworkServer.UnSpawn(m_ExplosionParticles.gameObject);
     }
 
+    [Server]
+    private void Explode(Vector3 position, Quaternion rotation)
+    {
+        m_ExplosionParticles.transform.position = position;
+        m_ExplosionParticles.transform.rotation = rotation;
+        m_ExplosionParticles.Play();
+        m_ExplosionAudio.Play();
+    }
 
+    [Client]
     private void OnTriggerEnter(Collider other)
     {
         // Find all the tanks in an area around the shell and damage them.
-
         Collider[] colliders = Physics.OverlapSphere(transform.position, m_ExplosionRadius, m_TankMask);
 
         for (int i = 0; i < colliders.Length; i++)
@@ -51,15 +63,13 @@ public class ShellExplosion : NetworkBehaviour, IPooledObject
             targetHealth.m_CurrentHealth = Mathf.Max(targetHealth.m_CurrentHealth - damage, 0);
         }
 
-        m_ExplosionParticles.transform.position = gameObject.transform.position;
-        m_ExplosionParticles.transform.rotation = gameObject.transform.rotation;
-        m_ExplosionParticles.Play();
-        m_ExplosionAudio.Play();
+        Explode(gameObject.transform.position, gameObject.transform.rotation);
 
         gameObject.SetActive(false);
+        Invoke(nameof(Deactivate), 0.5f);
     }
 
-
+    [Server]
     private float CalculateDamage(Vector3 targetPosition)
     {
         // Calculate the amount of damage a target should take based on it's position.
