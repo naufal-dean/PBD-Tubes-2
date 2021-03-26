62d2f35283858a804c0ba4e8e6b15ce446bc660c
naufal-dean <13518123@std.stei.itb.ac.id>
2021-03-27 00:32:24 +0700

    chore: add target enemy if near

--- Assets/Scripts/Soldier/SoldierMovement.cs
+++ Assets/Scripts/Soldier/SoldierMovement.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class SoldierMovement : NetworkBehaviour
 {
     public LayerMask m_TankMask;
-    public float m_TargetRadius = 10f;
+    public float m_TargetRadius = 15f;
 
     private TankBehaviour m_TankOwner;
     private Animator anim;
@@ -21,7 +21,26 @@ public class SoldierMovement : NetworkBehaviour
 
     void Update()
     {
-        if (isClient && m_TankOwner)
+        if (!isClient) return;
+
+        if (!m_TankOwner) return;
+
+        GameObject target = DetectEnemyPosition();
+
+        if (target)
+        {
+            if (Vector3.Distance(gameObject.transform.position, target.transform.position) <= 5f)
+            {
+                anim.SetTrigger("Stop");
+                nav.SetDestination(gameObject.transform.position);
+            }
+            else
+            {
+                anim.SetTrigger("Run");
+                nav.SetDestination(target.transform.position);
+            }
+        }
+        else if (m_TankOwner)
         {
             if (Vector3.Distance(gameObject.transform.position, m_TankOwner.gameObject.transform.position) <= 10f)
             {
@@ -34,6 +53,38 @@ public class SoldierMovement : NetworkBehaviour
                 nav.SetDestination(m_TankOwner.gameObject.transform.position);
             }
         }
+    }
+
+
+    private GameObject DetectEnemyPosition()
+    {
+        GameObject target = null;
+        float minDistance = float.MaxValue;
+
+        // Find all the tanks in an area around the soldier.
+        Collider[] colliders = Physics.OverlapSphere(transform.position, m_TargetRadius, m_TankMask);
+
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            Rigidbody targetRigidbody = colliders[i].GetComponent<Rigidbody>();
+            
+            if (!targetRigidbody)
+                continue;
+
+            TankBehaviour tankBehaviour = targetRigidbody.GetComponent<TankBehaviour>();
+
+            if (tankBehaviour && tankBehaviour == m_TankOwner)
+                continue;
+
+            float newDistance = Vector3.Distance(targetRigidbody.gameObject.transform.position, gameObject.transform.position);
+            if (newDistance < minDistance)
+            {
+                target = targetRigidbody.gameObject;
+                minDistance = newDistance;
+            }
+        }
+
+        return target;
     }
 
 
