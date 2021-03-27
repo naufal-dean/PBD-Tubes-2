cdffb666d91b43e6c88b690a7cf7c83831610ded
BaconLover307 <kresnadi30@gmail.com>
2021-03-27 23:53:54 +0700

    Finishing

--- Assets/Scripts/Managers/NetworkManagerTank.cs
+++ Assets/Scripts/Managers/NetworkManagerTank.cs
@@ -7,351 +7,343 @@ using UnityEngine.UI;
 
 public class NetworkManagerTank : NetworkManager
 {
-  public int m_NumRoundsToWin = 5;
-  public float m_StartDelay = 3f;
-  public float m_EndDelay = 3f;
-  public int m_MaxNumPlayers = 2;
-  public CameraControl m_CameraControl;
-  public Text m_MessageText;
-  public List<TankBehaviour> m_Tanks;
-  public NetworkManagerHUD networkManagerHUD;
-  public Transform[] m_SpawnPoints;  // need to be defined with minimum length m_MaxNumPlayers
+    public int m_NumRoundsToWin = 5;
+    public float m_StartDelay = 3f;
+    public float m_EndDelay = 3f;
+    public int m_MaxNumPlayers = 2;
+    public CameraControl m_CameraControl;
+    public Text m_MessageText;
+    public List<TankBehaviour> m_Tanks;
+    public NetworkManagerHUD networkManagerHUD;
+    public Transform[] m_SpawnPoints;  // need to be defined with minimum length m_MaxNumPlayers
 
-  private int m_RoundNumber;
-  private WaitForSeconds m_StartWait;
-  private WaitForSeconds m_EndWait;
-  private TankBehaviour m_RoundWinner;
-  private UIText m_UIText;
-  [HideInInspector] public TankBehaviour m_GameWinner;
-  [HideInInspector] public bool m_GameRunning = false;
+    // TODO: remove after spawn point is randomized
+    public Transform m_SpawnPoint;
 
-  MobFactory mobFactory;
 
-  public override void Start()
-  {
-    base.Start();
+    private int m_RoundNumber;
+    private WaitForSeconds m_StartWait;
+    private WaitForSeconds m_EndWait;
+    private TankBehaviour m_RoundWinner;
+    private UIText m_UIText;
+    [HideInInspector] public TankBehaviour m_GameWinner;
+    [HideInInspector] public bool m_GameRunning = false;
 
-    m_StartWait = new WaitForSeconds(m_StartDelay);
-    m_EndWait = new WaitForSeconds(m_EndDelay);
+    MobFactory mobFactory;
 
-    m_UIText = m_MessageText.GetComponent<UIText>();
-
-    mobFactory = MobFactory.Instance;
-  }
-
-  #region Client
-
-  [Client]
-  public void PauseGame()
-  {
-    if (m_GameRunning)
-      DisableTankControl();
-    networkManagerHUD.showGUI = true;
-    Debug.Log(networkManagerHUD.showGUI);
-
-  }
-
-  [Client]
-  public void ResumeGame()
-  {
-    if (m_GameRunning)
-      DisableTankControl();
-    networkManagerHUD.showGUI = false;
-  }
-
-  public void QuitGame()
-  {
-    // stop host if host mode
-    if (NetworkServer.active && NetworkClient.isConnected)
+    public override void Start()
     {
-      StopHost();
-    }
-    // stop client if client-only
-    else if (NetworkClient.isConnected)
-    {
-      StopClient();
-    }
-    // stop server if server-only
-    else if (NetworkServer.active)
-    {
-      StopServer();
+        base.Start();
+
+        m_StartWait = new WaitForSeconds(m_StartDelay);
+        m_EndWait = new WaitForSeconds(m_EndDelay);
+
+        m_UIText = m_MessageText.GetComponent<UIText>();
+
+        mobFactory = MobFactory.Instance;
     }
 
-    networkManagerHUD.showGUI = false;
-    gameObject.GetComponent<AudioSource>().Stop();
-    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
-  }
+    #region Client
 
-  #endregion
-
-  #region Server
-
-  public override void OnServerAddPlayer(NetworkConnection conn)
-  {
-    if (numPlayers >= m_MaxNumPlayers)
+    [Client]
+    public void PauseGame()
     {
-      print("Room is full");
-      return;
+        networkManagerHUD.showGUI = true;
+        Debug.Log(networkManagerHUD.showGUI);
+
     }
 
-    SpawnTank(conn, numPlayers);
-
-    // Start game if player number is enough
-    if (numPlayers == m_MaxNumPlayers)
+    [Client]
+    public void ResumeGame()
     {
-      StartGame();
-      StartCoroutine(GameLoop());
-      networkManagerHUD.showGUI = false;
-    }
-  }
-
-
-  public override void OnServerDisconnect(NetworkConnection conn)
-  {
-    TankBehaviour player = conn.identity.gameObject.GetComponent<TankBehaviour>();
-    m_Tanks.Remove(player);
-
-    base.OnServerDisconnect(conn);
-  }
-
-
-  [Server]
-  public void StartGame()
-  {
-    for (int i = 0; i < m_Tanks.Count; i++)
-    {
-      TankBehaviour player = m_Tanks[i];
-
-      // Set new spawn point
-      player.RpcSetSpawnPoint(m_SpawnPoints[i].position, m_SpawnPoints[i].rotation);
-
-      // Set camera to point player
-      player.RpcSetCameraTarget();
-
-      // Start game loop
-      StartCoroutine(GameLoop());
-
-      // Hide gui
-      // TODO: create RPC and hide on all client
-      networkManagerHUD.showGUI = false;
-    }
-  }
-
-
-  [Server]
-  private void SpawnTank(NetworkConnection conn, int spawnNumber)
-  {
-    // Instanstiate player
-    Transform startPos = GetTankSpawnPoint(spawnNumber);
-    TankBehaviour player =
-        Instantiate(playerPrefab, startPos.position, startPos.rotation).GetComponent<TankBehaviour>();
-
-    // Add player
-    NetworkServer.AddPlayerForConnection(conn, player.gameObject);
-
-    // Set player attribute
-    player.m_PlayerNumber = numPlayers;
-    player.m_PlayerColor = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
-    player.m_Control = false;
-    player.RpcSetSpawnPoint(startPos.position, startPos.rotation);
-
-    // Set camera to point player
-    player.RpcSetCameraTarget();
-
-    // TODO: remove, just for testing
-    //player.CmdSpawnMob(startPos.position, startPos.rotation);
-    //mobFactory.CmdSpawnMob(player);
-
-    // Save player to list
-    m_Tanks.Add(player);
-  }
-
-
-  [Server]
-  private Transform GetTankSpawnPoint(int spawnNumber)
-  {
-    // TODO: randomize spawn point
-
-    return m_SpawnPoints[spawnNumber];
-  }
-
-
-  [Server]
-  private IEnumerator GameLoop()
-  {
-    yield return StartCoroutine(RoundStarting());
-    yield return StartCoroutine(RoundPlaying());
-    yield return StartCoroutine(RoundEnding());
-
-    if (m_GameWinner != null)
-    {
-      SceneManager.LoadScene(0);
-    }
-    else
-    {
-      StartCoroutine(GameLoop());
-    }
-  }
-
-
-  [Server]
-  private IEnumerator RoundStarting()
-  {
-    ResetAllTanks();
-    ResetAllInfantry();
-    DisableTankControl();
-
-    m_CameraControl.SetStartPositionAndSize();
-
-    m_RoundNumber++;
-    m_UIText.RpcSetMessageText("ROUND " + m_RoundNumber);
-
-    yield return m_StartWait;
-  }
-
-
-  [Server]
-  private IEnumerator RoundPlaying()
-  {
-    m_GameRunning = true;
-    EnableTankControl();
-
-    m_UIText.RpcSetMessageText(string.Empty);
-
-    while (!OneTankLeft())
-    {
-      yield return null;
+        networkManagerHUD.showGUI = false;
     }
 
-  }
-
-
-  [Server]
-  private IEnumerator RoundEnding()
-  {
-    m_GameRunning = false;
-
-    DisableTankControl();
-
-    m_RoundWinner = null;
-
-    m_RoundWinner = GetRoundWinner();
-
-    if (m_RoundWinner != null)
-      m_RoundWinner.m_Wins++;
-
-    m_GameWinner = GetGameWinner();
-
-    string message = EndMessage();
-    m_UIText.RpcSetMessageText(message);
-
-    yield return m_EndWait;
-  }
-
-
-  [Server]
-  private bool OneTankLeft()
-  {
-
-    int numTanksLeft = 0;
-
-    for (int i = 0; i < m_Tanks.Count; i++)
+    [Client]
+    public void QuitGame()
     {
-      if (m_Tanks[i].gameObject.activeSelf)
-        numTanksLeft++;
+        // stop host if host mode
+        if (NetworkServer.active && NetworkClient.isConnected)
+        {
+            StopHost();
+        }
+        // stop client if client-only
+        else if (NetworkClient.isConnected)
+        {
+            StopClient();
+        }
+        // stop server if server-only
+        else if (NetworkServer.active)
+        {
+            StopServer();
+        }
+
+
+        networkManagerHUD.showGUI = false;
     }
 
-    return numTanksLeft <= 1;
-  }
+    #endregion
 
+    #region Server
 
-  [Server]
-  private TankBehaviour GetRoundWinner()
-  {
-    for (int i = 0; i < m_Tanks.Count; i++)
+    public override void OnServerAddPlayer(NetworkConnection conn)
     {
-      if (m_Tanks[i].gameObject.activeSelf)
-        return m_Tanks[i];
+        if (numPlayers >= m_MaxNumPlayers)
+        {
+            print("Room is full");
+            return;
+        }
+
+        SpawnTank(conn, numPlayers);
+
+        // Start game if player number is enough
+        if (numPlayers == m_MaxNumPlayers)
+        {
+            StartCoroutine(GameLoop());
+            networkManagerHUD.showGUI = false;
+        }
     }
 
-    return null;
-  }
 
-
-  [Server]
-  private TankBehaviour GetGameWinner()
-  {
-    for (int i = 0; i < m_Tanks.Count; i++)
+    public override void OnServerDisconnect(NetworkConnection conn)
     {
-      if (m_Tanks[i].m_Wins == m_NumRoundsToWin)
-        return m_Tanks[i];
+        TankBehaviour player = conn.identity.gameObject.GetComponent<TankBehaviour>();
+        m_Tanks.Remove(player);
+
+        base.OnServerDisconnect(conn);
     }
 
-    return null;
-  }
 
-
-  [Server]
-  private string EndMessage()
-  {
-    string message = "DRAW!";
-
-    if (m_RoundWinner != null)
-      message = m_RoundWinner.m_ColoredPlayerText + " WINS THE ROUND!";
-
-    message += "\n\n\n\n";
-
-    for (int i = 0; i < m_Tanks.Count; i++)
+    [Server]
+    public void StartGame()
     {
-      message += m_Tanks[i].m_ColoredPlayerText + ": " + m_Tanks[i].m_Wins + " WINS\n";
+        for (int i = 0; i < m_Tanks.Count; i++)
+        {
+            TankBehaviour player = m_Tanks[i];
+ 
+            // Set new spawn point
+            player.RpcSetSpawnPoint(m_SpawnPoints[i].position, m_SpawnPoints[i].rotation);
+
+            // Set camera to point player
+            player.RpcSetCameraTarget();
+
+            // Start game loop
+            StartCoroutine(GameLoop());
+
+            // Hide gui
+            // TODO: create RPC and hide on all client
+            networkManagerHUD.showGUI = false;
+        }
     }
 
-    if (m_GameWinner != null)
-      message = m_GameWinner.m_ColoredPlayerText + " WINS THE GAME!";
 
-    return message;
-  }
-
-
-  [Server]
-  private void ResetAllTanks()
-  {
-    for (int i = 0; i < m_Tanks.Count; i++)
+    [Server]
+    private void SpawnTank(NetworkConnection conn, int spawnNumber)
     {
-      m_Tanks[i].RpcReset();
-    }
-  }
+        // Instanstiate player
+        Transform startPos = GetTankSpawnPoint(spawnNumber);
+        TankBehaviour player =
+            Instantiate(playerPrefab, startPos.position, startPos.rotation).GetComponent<TankBehaviour>();
 
-  [Server]
-  private void ResetAllInfantry()
-  {
-    GameObject[] infantries = GameObject.FindGameObjectsWithTag("Player");
-    foreach (GameObject infantry in infantries)
+        // Add player
+        NetworkServer.AddPlayerForConnection(conn, player.gameObject);
+
+        // Set player attribute
+        player.m_PlayerNumber = numPlayers;
+        player.m_PlayerColor = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+        player.m_Control = false;
+        player.RpcSetSpawnPoint(startPos.position, startPos.rotation);
+
+        // Set camera to point player
+        player.RpcSetCameraTarget();
+
+        // TODO: remove, just for testing
+        //player.CmdSpawnMob(startPos.position, startPos.rotation);
+        //mobFactory.CmdSpawnMob(player);
+
+        // Save player to list
+        m_Tanks.Add(player);
+    }
+
+
+    [Server]
+    private Transform GetTankSpawnPoint(int spawnNumber)
     {
-      infantry.SetActive(false);
-      NetworkServer.UnSpawn(infantry);
+        // TODO: randomize spawn point
+
+        return m_SpawnPoints[spawnNumber];
     }
-  }
 
 
-  [Server]
-  private void EnableTankControl()
-  {
-    for (int i = 0; i < m_Tanks.Count; i++)
+    [Server]
+    private IEnumerator GameLoop()
     {
-      m_Tanks[i].m_Control = true;
+        yield return StartCoroutine(RoundStarting());
+        yield return StartCoroutine(RoundPlaying());
+        yield return StartCoroutine(RoundEnding());
+
+        if (m_GameWinner != null)
+        {
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+            StartCoroutine(GameLoop());
+        }
     }
-  }
 
 
-  [Server]
-  private void DisableTankControl()
-  {
-    for (int i = 0; i < m_Tanks.Count; i++)
+    [Server]
+    private IEnumerator RoundStarting()
     {
-      m_Tanks[i].m_Control = false;
-    }
-  }
+        ResetAllTanks();
+        DisableTankControl();
 
-  #endregion
+        m_CameraControl.SetStartPositionAndSize();
+
+        m_RoundNumber++;
+        m_UIText.RpcSetMessageText("ROUND " + m_RoundNumber);
+
+        yield return m_StartWait;
+    }
+
+
+    [Server]
+    private IEnumerator RoundPlaying()
+    {
+        m_GameRunning = true;
+        EnableTankControl();
+
+        m_UIText.RpcSetMessageText(string.Empty);
+
+        while (!OneTankLeft())
+        {
+            yield return null;
+        }
+
+    }
+
+
+    [Server]
+    private IEnumerator RoundEnding()
+    {
+        m_GameRunning = false;
+
+        DisableTankControl();
+
+        m_RoundWinner = null;
+
+        m_RoundWinner = GetRoundWinner();
+
+        if (m_RoundWinner != null)
+            m_RoundWinner.m_Wins++;
+
+        m_GameWinner = GetGameWinner();
+
+        string message = EndMessage();
+        m_UIText.RpcSetMessageText(message);
+
+        yield return m_EndWait;
+    }
+
+
+    [Server]
+    private bool OneTankLeft()
+    {
+
+        int numTanksLeft = 0;
+
+        for (int i = 0; i < m_Tanks.Count; i++)
+        {
+            if (m_Tanks[i].gameObject.activeSelf)
+                numTanksLeft++;
+        }
+
+        return numTanksLeft <= 1;
+    }
+
+
+    [Server]
+    private TankBehaviour GetRoundWinner()
+    {
+        for (int i = 0; i < m_Tanks.Count; i++)
+        {
+            if (m_Tanks[i].gameObject.activeSelf)
+                return m_Tanks[i];
+        }
+
+        return null;
+    }
+
+
+    [Server]
+    private TankBehaviour GetGameWinner()
+    {
+        for (int i = 0; i < m_Tanks.Count; i++)
+        {
+            if (m_Tanks[i].m_Wins == m_NumRoundsToWin)
+                return m_Tanks[i];
+        }
+
+        return null;
+    }
+
+
+    [Server]
+    private string EndMessage()
+    {
+        string message = "DRAW!";
+
+        if (m_RoundWinner != null)
+            message = m_RoundWinner.m_ColoredPlayerText + " WINS THE ROUND!";
+
+        message += "\n\n\n\n";
+
+        for (int i = 0; i < m_Tanks.Count; i++)
+        {
+            message += m_Tanks[i].m_ColoredPlayerText + ": " + m_Tanks[i].m_Wins + " WINS\n";
+        }
+
+        if (m_GameWinner != null)
+            message = m_GameWinner.m_ColoredPlayerText + " WINS THE GAME!";
+
+        return message;
+    }
+
+    [Server]
+    private void ResetInfantry()
+    {
+        GameObject[] infantries = GameObject.FindGameObjectsWithTag("Infantry");
+    }
+
+    [Server]
+    private void ResetAllTanks()
+    {
+        for (int i = 0; i < m_Tanks.Count; i++)
+        {
+            m_Tanks[i].RpcReset();
+        }
+    }
+
+
+    [Server]
+    private void EnableTankControl()
+    {
+        for (int i = 0; i < m_Tanks.Count; i++)
+        {
+            m_Tanks[i].m_Control = true;
+        }
+    }
+
+
+    [Server]
+    private void DisableTankControl()
+    {
+        for (int i = 0; i < m_Tanks.Count; i++)
+        {
+            m_Tanks[i].m_Control = false;
+        }
+    }
+
+    #endregion
 }
